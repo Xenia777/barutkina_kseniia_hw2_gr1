d2a92c84f1f0e696b046af3be78d7fc3c3534904
Xenia777 <109750310+Xenia777@users.noreply.github.com>
2022-09-03 09:52:46 +0000

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,37 @@
 import java.util.Scanner;
+import java.util.Random;
 
-public class Solution {
 
-    public static void Data(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите ваш возраст");
-        int age = sc.nextInt();;
-        System.out.println("Введите температуру на улице");
-        int temp = sc.nextInt();;
-        if(20 >= age && temp >= 0 ){
-            System.out.println("Можно идти гулять");
-        }
+public class Program {
 
-}}
+    public static int generateRandomAge(){
+        Random rnd = new Random(System.currentTimeMillis());
+        int number = 0 + rnd.nextInt(10 - 0 + 1);
+        System.out.println(number);
+        return number;
+    }
+
+
+    public static void main(String[] args) {
+
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Введите ваш возраст");
+                int age = new Program().generateRandomAge();
+                System.out.println("Введите температуру на улице");
+                int temp = sc.nextInt();
+                if(age >= 20 && age <= 45 && temp >= -20 && temp <= 30) {
+                    System.out.println("Можно идти гулять");}
+                else if(age < 20 && temp >= 0 && temp <= 28){
+                    System.out.println("Можно идти гулять");}
+                else if(age > 45 && temp >= -10 && temp <= 25){
+                    System.out.println("Можно идти гулять");}
+                else{
+                    System.out.println("Отсавайтесь дома");}
+
+
+            }}
+
+
+
+
+
